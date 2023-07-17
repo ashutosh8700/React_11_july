6c95aa80925e6921ebed0346c92b9a5dcb74aec7
Ashutosh Dangi <ashutoshdangirdps@gmail.com>
2023-07-17 22:19:31 +0530

    created component Tile  and called in function App

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// created component name Tile
+function Tile(){
+  return(
+    <div>
+      <span>x</span>
+    </div>
+  )
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <h1>Welcome to react</h1>
+      <p>How are u doing</p>
+      {/* calling the Tile component this componenet can be used multiple times*/}
+      <Tile/>
     </div>
   );
 }
