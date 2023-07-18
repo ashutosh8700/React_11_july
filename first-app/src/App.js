18e4a322d509667d0f0b66c5798a18a6574f3f6d
Ashutosh Dangi <ashutoshdangirdps@gmail.com>
2023-07-18 17:37:29 +0530

    Import and export components

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -7,12 +7,15 @@ import Tile from './Tile';
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Welcome to react</h1>
       <p>How are u doing</p>
+      {/* In jsx we use className instead of class */}
+      <span className = "class"> test </span> 
       {/* calling the Tile component this componenet can be used multiple times*/}
       <Tile/>
-    </div>
+      
+    </>
   );
 }
 
--- first-app/src/Tile.js
+++ first-app/src/Tile.js
@@ -1,9 +1,11 @@
 // created component name Tile
 function Tile(){
     return(
-      <div>
+      <> 
+      
         <span>x</span>
-      </div>
+        
+      </>
     )
   }
 // exporting Tile
